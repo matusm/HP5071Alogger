6c0e9683548e3898c8a4a099194cbf84e3eb3d28
Michael Matus <michael@matus.at>
2020-03-10 11:03:51 +0000

    Settings new

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -25,19 +25,19 @@ namespace HP5071Alogger.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("COM16")]
-        public string ComPort1 {
+        [global::System.Configuration.DefaultSettingValueAttribute("1")]
+        public int NumberOfStandards {
             get {
-                return ((string)(this["ComPort1"]));
+                return ((int)(this["NumberOfStandards"]));
             }
         }
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("COM1")]
-        public string ComPort2 {
+        [global::System.Configuration.DefaultSettingValueAttribute(" ; ")]
+        public string CsvDelimiter {
             get {
-                return ((string)(this["ComPort2"]));
+                return ((string)(this["CsvDelimiter"]));
             }
         }
         
@@ -79,28 +79,19 @@ namespace HP5071Alogger.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("SY5071A")]
-        public string Name1 {
+        [global::System.Configuration.DefaultSettingValueAttribute("COM7")]
+        public string ComPort1 {
             get {
-                return ((string)(this["Name1"]));
+                return ((string)(this["ComPort1"]));
             }
         }
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("AG5071A")]
-        public string Name2 {
+        [global::System.Configuration.DefaultSettingValueAttribute("COM1")]
+        public string ComPort2 {
             get {
-                return ((string)(this["Name2"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("HP5071A")]
-        public string Name3 {
-            get {
-                return ((string)(this["Name3"]));
+                return ((string)(this["ComPort2"]));
             }
         }
         
@@ -128,6 +119,51 @@ namespace HP5071Alogger.Properties {
         public string ComPort5 {
             get {
                 return ((string)(this["ComPort5"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("SY5071A")]
+        public string Name1 {
+            get {
+                return ((string)(this["Name1"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("AG5071A")]
+        public string Name2 {
+            get {
+                return ((string)(this["Name2"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("HP5071A")]
+        public string Name3 {
+            get {
+                return ((string)(this["Name3"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("MM00004")]
+        public string Name4 {
+            get {
+                return ((string)(this["Name4"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("MM00005")]
+        public string Name5 {
+            get {
+                return ((string)(this["Name5"]));
             }
         }
         
@@ -173,42 +209,6 @@ namespace HP5071Alogger.Properties {
         public string LogFileBaseName5 {
             get {
                 return ((string)(this["LogFileBaseName5"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(" ; ")]
-        public string CsvDelimiter {
-            get {
-                return ((string)(this["CsvDelimiter"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("MM00004")]
-        public string Name4 {
-            get {
-                return ((string)(this["Name4"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("MM00005")]
-        public string Name5 {
-            get {
-                return ((string)(this["Name5"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2")]
-        public int NumberOfStandards {
-            get {
-                return ((int)(this["NumberOfStandards"]));
             }
         }
     }
